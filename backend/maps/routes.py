e65f905c0ef14f0594aac8b5fc9cb13454395c5e
Rachit <rachitbhargava99@gmail.com>
2020-02-01 00:19:36 -0500

    Ability to add new query result added

--- backend/maps/routes.py
+++ backend/maps/routes.py
@@ -1,7 +1,8 @@
 import json
 from datetime import datetime
+import requests
 
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
 
 from backend import db
 from backend.models import User, Query
@@ -62,6 +63,48 @@ def create_new_query():
 #  other people's queries
 
 
-@maps.route('/map/query/result')
+@maps.route('/map/query/result', methods=['POST'])
 def create_query_result():
+    """
+    Creates new query for the user
 
+    Method Type
+    -----------
+    POST
+
+    JSON Parameters
+    ---------------
+    auth_token : str
+        Authentication of the logged in user
+    query_id : int
+        ID of the query for which we need to
+
+    Restrictions
+    ------------
+    User must be logged in
+    Query must belong to the logged in user
+
+    JSON Returns
+    ------------
+    status : int
+        Status code representing success status of the request
+    message : str
+        Message explaining the response status
+    polyline : str
+        Polyline representing points to visit - to be used by Google Maps
+    """
+    request_json = request.get_json()
+    auth_token = request_json['auth_token']
+    user = User.verify_auth_token(auth_token)
+    if user is None:
+        return token_expiration_json_response
+    query_id = request_json['query_id']
+    query = Query.query.filter_by(id=query_id).first()
+    if query is None:
+        return json.dumps({'status': 3, 'message': "Requested query does not exist"})
+    direction_raw_result = requests.post(f"{current_app.config['MAPS_DIRECTION_BASE']}?origin={query.entry_o}" +
+                                         f"&destination={query.entry_d}&mode=driving" +
+                                         f"&key=AIzaSyDYUOtB-7CuX7Ex_BkbpOW4jP7redSjtTg")
+    direction_result = direction_raw_result.json()
+    polyline = direction_result['routes'][0]['legs'][0]['polyline']['points']
+    return json.dumps({'status': 0, 'message': "Basic Route Created Successfully", 'polyline': polyline})
